eeeb83b161ba0dd84c727a66475beb540e210dc7
B3none <24966460+B3none@users.noreply.github.com>
2024-01-02 22:44:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# CS2 {PLUGIN_NAME}
+{PLUGIN_NAME} plugin written in C# for CounterStrikeSharp
+
+## Progress
+- [x] These will
+- [x] Help you
+- [ ] Keep people
+- [x] Up to date
+- [ ] With your progress
+
+## Installation
+1. Download the zip file from the latest release, and extract the contents into your `counterstrikesharp/plugins` directory.
+
+## Setup for local development
+Download the latest release of CounterStrikeSharp, and put the contents into the `CounterStrikeSharp` directory.
---
+++ TemplatePlugin.cs
@@ -0,0 +1,30 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace TemplatePlugin;
+
+[MinimumApiVersion(129)]
+public class TemplatePlugin : BasePlugin
+{
+    private const string Version = "1.0.0";
+    
+    public override string ModuleName => "{PLUGIN_NAME} Plugin";
+    public override string ModuleVersion => Version;
+    public override string ModuleAuthor => "{PLUGIN_AUTHOR}";
+    public override string ModuleDescription => "{PLUGIN_DESCRIPTION}";
+    
+    public static readonly string LogPrefix = $"[PLUGIN_NAME {Version}] ";
+    public static readonly string MessagePrefix = $"[{ChatColors.Green}PLUGIN_NAME{ChatColors.White}] ";
+    
+    [GameEventHandler]
+    public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        Console.WriteLine($"{LogPrefix}OnRoundStart event fired!");
+        Server.PrintToChatAll($"{MessagePrefix}OnRoundStart event fired!");
+
+        return HookResult.Continue;
+    }
+}
